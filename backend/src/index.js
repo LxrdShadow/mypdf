9b66c2e4cacedcd8a4bbcfad93cf10760ab946eb
LxrdShadow <andritiana.idealy@gmail.com>
2026-01-19 17:07:06 +0300

    feat: configure dotenv integration

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,13 @@
+require("dotenv").config();
 const express = require("express");
+
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
     res.send("Hello World from Docker!");
 });
 
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
 });
