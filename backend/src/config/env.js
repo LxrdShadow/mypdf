bd50d150b3629f96b580b84de6c223eb8adac975
LxrdShadow <andritiana.idealy@gmail.com>
2026-01-19 21:31:28 +0300

    feat(config): add env variables to a config object

---
+++ backend/src/config/env.js
@@ -0,0 +1,7 @@
+module.exports = {
+    PORT: process.env.PORT || 3000,
+    NODE_ENV: process.env.NODE_ENV || "development",
+
+    STIRLING_API_URL: process.env.STIRLING_API_URL,
+    STIRLING_API_KEY: process.env.STIRLING_API_KEY,
+};
