76b5c418fac69f7271fa7b8c4a3f28ad3097033c
LxrdShadow <andritiana.idealy@gmail.com>
2026-01-20 15:34:09 +0300

    feat(pdf): add compressPdf function in the controller

--- backend/src/modules/pdf/Pdf.controller.js
+++ backend/src/modules/pdf/Pdf.controller.js
@@ -14,3 +14,17 @@ export async function mergePdfs(req, res, next) {
         next(error);
     }
 }
+
+export async function compressPdf(req, res, next) {
+    try {
+        const pdfService = new PdfService(axios);
+        const pdfStream = await pdfService.compress(req.file);
+
+        res.setHeader("Content-Type", "application/pdf");
+        res.setHeader("Content-Disposition", "attachment; filename=compressed.pdf");
+
+        pdfStream.pipe(res);
+    } catch (error) {
+        next(error);
+    }
+}
