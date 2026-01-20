c106672a377f28926ff75f51bc82d84580fc062e
LxrdShadow <andritiana.idealy@gmail.com>
2026-01-20 15:53:19 +0300

    feat(pdf): add compress pdf route

--- backend/src/modules/pdf/Pdf.routes.js
+++ backend/src/modules/pdf/Pdf.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { mergePdfs } from "./Pdf.controller.js";
+import { compressPdf, mergePdfs } from "./Pdf.controller.js";
 import upload from "../../shared/upload/index.js";
 
 const router = Router();
 
 router.post("/merge", upload.array("files", 10), mergePdfs);
+router.post("/compress", upload.single("file"), compressPdf);
 
 export default router;
